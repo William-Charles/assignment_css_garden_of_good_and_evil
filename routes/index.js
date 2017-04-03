a1d6ad72475033fdce34b9cf0e562ef0035dcf02
William Whitworth <william10charles@gmail.com>
2017-04-03 11:33:22 -0600

    linking form radio to router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//pull out the value send back with class change with interpleation
+//styles model
+// if cookie() is evil chage class of body dark background
+
+router.get("/", function(req, res, next) {
+  //likes = generateLikes(req.cookies);
+  res.render("index");
+});
+
+router.post("/", function(req, res, next) {
+  const id = req.params.goodEvil;
+  console.log(id);
+  res.render("index");
 });
 
 module.exports = router;
