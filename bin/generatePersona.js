f66c3a33327a52bac2d07752e296f3831ed672a2
William Whitworth <william10charles@gmail.com>
2017-04-03 14:43:12 -0600

    struggling with computer

--- bin/generatePersona.js
+++ bin/generatePersona.js
@@ -1,20 +1,19 @@
-const good = require('../models/goodPersona')
-const evil = require('../models/evilPersona')
-
-
+const good = require("../models/goodPersona");
+const evil = require("../models/evilPersona");
 
 function generatePersona(cookie) {
   let persona = {};
-  if (cookie.goodEvil === 'good') {
+  if (cookie.goodEvil === "good") {
     persona = good;
   } else {
     persona = evil;
   }
-  persona.favFood = cookie.favFood;
-  persona.color = cookie.color;
+  //persona.likes.push(cookie.favFood, cookie.color);
+  // persona.favFood = cookie.favFood;
+  // persona.color = cookie.color;
   persona.insanity = cookie.insanity;
   //possibly insanity variation function
-  return persona
+  return persona;
 }
 
-module.exports = generatePersona
+module.exports = generatePersona;
